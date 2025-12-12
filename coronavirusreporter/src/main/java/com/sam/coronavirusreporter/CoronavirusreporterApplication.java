29b3bcdeba77c4fdc2d78aa0d3086765ffa56770
sambeet.nayak.ext <sambeet.nayak.ext@siemens-energy.com>
2025-12-12 16:17:16 +0530

    cleanup..

--- coronavirusreporter/src/main/java/com/sam/coronavirusreporter/CoronavirusreporterApplication.java
+++ coronavirusreporter/src/main/java/com/sam/coronavirusreporter/CoronavirusreporterApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 
-//AAAAAAAA
 @SpringBootApplication
 @EnableScheduling
 public class CoronavirusreporterApplication {
