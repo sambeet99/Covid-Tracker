097d4a874aaf19f9d9a81c87f66914e9008650ed
sambeet.nayak.ext <sambeet.nayak.ext@siemens-energy.com>
2025-12-12 20:11:41 +0530

    main div

--- coronavirusreporter/src/test/java/com/sam/coronavirusreporter/CoronavirusreporterApplicationTests.java
+++ coronavirusreporter/src/test/java/com/sam/coronavirusreporter/CoronavirusreporterApplicationTests.java
@@ -4,8 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 
-//dfsdfd
-//sdfs
+
 // main -div
 @SpringBootTest
 class CoronavirusreporterApplicationTests {
