f534db7f2aab41e6440f017b54896a58ab2da5fe
sambeet.nayak.ext <sambeet.nayak.ext@siemens-energy.com>
2025-12-12 15:36:00 +0530

    aaa

--- coronavirusreporter/src/main/java/com/sam/coronavirusreporter/CoronavirusreporterApplication.java
+++ coronavirusreporter/src/main/java/com/sam/coronavirusreporter/CoronavirusreporterApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 
-
+//AAAAA
 @SpringBootApplication
 @EnableScheduling
 public class CoronavirusreporterApplication {
